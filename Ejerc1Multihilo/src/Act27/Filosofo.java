da79f805230e0435e82b9aee802ec21e66cf735f
Miguel Angel Ruiz Aguilar <tedaxjr@gmail.com>
2024-12-05 14:46:42 +0100

    Solucion ejercicioFilosofos.

--- Ejerc1Multihilo/src/Act27/Filosofo.java
+++ Ejerc1Multihilo/src/Act27/Filosofo.java
@@ -6,11 +6,14 @@ package Act27;
 
 import java.util.Random;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Filosofo extends Thread {
     private final int id;
     private final Semaphore tenedorIzquierdo;
     private final Semaphore tenedorDerecho;
+    private final static Lock cogiendoTenedores =new ReentrantLock(true);
 
     public Filosofo(int id, Semaphore tenedorIzquierdo, Semaphore tenedorDerecho) {
         this.id = id;
@@ -30,19 +33,27 @@ public class Filosofo extends Thread {
 
                 // Intentar tomar los tenedores
                 System.out.println("Filósofo "+id+" está intentando tomar los tenedores.");
-                tenedorIzquierdo.acquire();
-                tenedorDerecho.acquire();
-
+                
+                cogiendoTenedores.lock();
+                try {
+                    // Aqui hay que modficiar para cuando todos los filosofos quieren coger el mismo tenedor.
+                    tenedorIzquierdo.acquire();
+                    
+                    tenedorDerecho.acquire();
+                } finally {
+                    cogiendoTenedores.unlock();
+                }
                 // Comer
                 System.out.println("Filósofo "+id+" está comiendo.");
                 int tiempoComiendo=random.nextInt(4)+3;
                 Thread.sleep(tiempoComiendo*1000);
+                System.out.println("Filósofo "+id+" ha comido.");
 
-
+                System.out.println("Filósofo "+id+" terminó de comer y soltó los tenedores.");
                 // Soltar los tenedores
                 tenedorIzquierdo.release();
                 tenedorDerecho.release();
-                System.out.println("Filósofo "+id+" terminó de comer y soltó los tenedores.");
+                
             }
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
